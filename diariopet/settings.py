a96c96bd9249d9c2fc7ac87f13f61466841347ea
Leonardo Menezes <leonardosmenezes.ssz@gmail.com>
2020-10-20 00:32:35 -0300

    Update docker-compose.yml with wait-for-it.sh

--- diariopet/settings.py
+++ diariopet/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'diariopet.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'diario-pet',
+        'NAME': 'diario_pet',
         'USER': 'leomenezes',
         'PASSWORD': 'meiko',
         'HOST': 'db',
