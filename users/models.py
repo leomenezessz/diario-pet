d8769e29ad30ea4364bcce5fc506c8d7289255ef
Leonardo Menezes <leonardosmenezes.ssz@gmail.com>
2020-10-23 03:51:44 -0300

    Remove some fields from user model

--- users/models.py
+++ users/models.py
@@ -4,10 +4,7 @@ from pets.models import Pet
 
 class User(models.Model):
     picture = models.ImageField(upload_to="users/images/")
-    first_name = models.CharField(max_length=100)
     second_name = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
     cellphone = models.IntegerField()
     birthday = models.DateField()
     address = models.CharField(max_length=200)
